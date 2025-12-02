05c761f0a37b9c2e3993c65fd512a9b70df6efd3
abx393 <abandari99@gmail.com>
2025-12-02 16:24:18 -0600

    invoke llm on stack trace with structured output

---
+++ main.py
@@ -0,0 +1,43 @@
+from enum import Enum
+import networkx as nx
+
+from langchain_openai import ChatOpenAI
+
+from typing import TypedDict, Literal
+
+from graph_builder import build_graph, build_scc_dag, compute_sccs, extract_edges_from_trace
+from log_parser import get_stacktrace_from_logs
+
+class RCAResult(TypedDict):
+    root_cause_service: ServiceType
+
+def invoke_llm(message):
+    llm = ChatOpenAI(model="gpt-4o-mini")
+    structured_llm = llm.with_structured_output(RCAResult)
+
+    result = structured_llm.invoke(message)
+    print(result)
+
+def main():
+    traces_path = "dataset/RE3-OB/cartservice_f1/1/traces.csv"   # your file name
+    edges = extract_edges_from_trace(traces_path)
+
+    print("Extracted edges:")
+    for e in edges:
+        print(e)
+
+    G = build_graph(edges)
+    scc_list, scc_map = compute_sccs(G)
+    scc_dag = build_scc_dag(G, scc_map)
+
+    ServiceType = Enum("ServiceType", {node : node for node in scc_dag.nodes})
+
+    log_path = "dataset/RE3-OB/cartservice_f1/1/logs.csv"
+    stacktraces = get_stacktrace_from_logs(log_path)
+    first_stacktrace = list(stacktraces)[0]
+
+    msg = f"Identify the root cause of the bug given the following stacktrace: {first_stacktrace}"
+    invoke_llm(msg)
+
+if __name__ == "__main__":
+    main()
